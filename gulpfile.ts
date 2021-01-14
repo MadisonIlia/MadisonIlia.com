8fb5dbdb36482f78b3062f72d4c70ecbff18a5dc
Bronley <bronley@gmail.com>
2021-01-14 15:34:57 -0500

    add build task

---
+++ gulpfile.ts
@@ -0,0 +1,51 @@
+import * as gulp from 'gulp';
+import * as ejs from 'gulp-ejs';
+import * as server from 'gulp-server-livereload';
+import * as rimraf from 'rimraf';
+
+function clean(done) {
+    rimraf('dist', {}, done);
+}
+
+function templates() {
+    return gulp.src('src/*.html')
+        .pipe(ejs())
+        .pipe(gulp.dest('./dist'))
+}
+
+function assets() {
+    return gulp.src('public/**/*')
+        .pipe(gulp.dest('dist'));
+}
+
+const build = gulp.series(
+    clean,
+    gulp.parallel(
+        templates,
+        assets
+    )
+);
+
+function watch() {
+    gulp.watch('src/**/*', templates);
+    gulp.watch('public/**/*', assets);
+}
+
+function serve() {
+    gulp.src('dist')
+        .pipe(server({
+            livereload: true,
+            open: true,
+            host: '0.0.0.0'
+        }))
+}
+
+gulp.task('start', gulp.series(
+    build,
+    gulp.parallel(
+        watch,
+        serve
+    )
+));
+
+gulp.task('build', build);
